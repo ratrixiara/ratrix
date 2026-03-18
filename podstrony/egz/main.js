4d217e3e0d1f8af39c392860854fc564e53b998d
ratrixiara <danielkarpiuk14@gmail.com>
2026-03-18 12:38:07 +0100

    Update main.js

--- podstrony/egz/main.js
+++ podstrony/egz/main.js
@@ -12,6 +12,7 @@ function lalala() {
     const a1 = document.getElementById("a1")
     const a2 = document.getElementById("a2")
     const a3 = document.getElementById("a3")
+    const a4 = document.getElementById("a4")
 
   
 
@@ -37,5 +38,9 @@ function lalala() {
 
             a3.style.display = "block"
             break
+        case 4:
+            a3.style.display = "none"
+
+            a4.style.display = "block"
     }
 }
