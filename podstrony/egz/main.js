d0c81e606f7dbe9c89af24be00fefbce97d556aa
ratrixiara <danielkarpiuk14@gmail.com>
2026-03-18 11:16:03 +0100

    Add files via upload

---
+++ podstrony/egz/main.js
@@ -0,0 +1,41 @@
+let aaa = 1
+
+
+function lalala() {
+
+    aaa += 1
+
+    if (aaa > 3){ 
+        aaa = 1
+    }
+
+    const a1 = document.getElementById("a1")
+    const a2 = document.getElementById("a2")
+    const a3 = document.getElementById("a3")
+
+  
+
+    switch (aaa) {
+        //trch spaghetti ale nie wazne  
+        case 1:
+
+            a3.style.display = "none"
+
+            a1.style.display = "block"
+            break
+
+        case 2:
+
+            a1.style.display = "none"
+
+            a2.style.display = "block"
+            break
+
+        case 3:
+
+            a2.style.display = "none"
+
+            a3.style.display = "block"
+            break
+    }
+}
